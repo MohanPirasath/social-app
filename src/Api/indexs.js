6b13fdacf9a4c7fcabe152364e7c75d5e79d6b77
Mohanpirasath <mahendran.mohanmass2000@gmail.com>
2022-08-12 09:32:01 +0530

    final

--- src/Api/indexs.js
+++ src/Api/indexs.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 
-const API= axios.create({baseURL:"http://localhost:5000"})
+const API= axios.create({baseURL:"https://file-it-app.herokuapp.com"})
+// const API= axios.create({baseURL:"http://localhost:5000"})
 // const url = "http://localhost:5000"
 
  API.interceptors.request.use((req)=>{
